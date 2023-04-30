7d7dd8155f1fae8648f796ad10d7d73201af58bd
gdn992 <66187782+gdn992@users.noreply.github.com>
2023-05-01 01:10:20 +0200

    Update lagrange.py
    made it able to print out the polinom of the expression

--- lagrange.py
+++ lagrange.py
@@ -10,21 +10,17 @@ class Vertex:
 def lagrange_interpolation(vertex_list: list[Vertex]):
     sub_polinom_list: list[function] = []
 
-    for i, vertex in enumerate(vertex_list):
-        x = vertex.x
-        y = vertex.y
+    for vertex in vertex_list:
         sub_polinom_list.append(
-            lambda X, x=x, y=y: get_subpolinoms(X, x, y, vertex_list)
+            lambda X, x=vertex.x, y=vertex.y: get_subpolinoms(
+                X, x, y, vertex_list
+            )
         )
 
     def sum(x: float):
         result = 0
-
-        print(('-' * 10) + 'subPolinoms' + ('-' * 10))
         for polinom in sub_polinom_list:
             result += polinom(x)
-        print('-' * 31)
-
         return result
 
     return sum
@@ -32,10 +28,41 @@ def lagrange_interpolation(vertex_list: list[Vertex]):
 
 def get_subpolinoms(X: float, x: float, y: float, vertex_list: list[Vertex]):
     L: float = 1
-    Ln: list[str] = []
-    for pont in vertex_list:
-        if x != pont.x:
-            Ln.append('((x-'+str(pont.x)+')/('+str(x)+'-'+str(pont.x)+'))')
-            L *= (X-pont.x)/(x-pont.x)
-    print('*'.join(Ln).replace('--', '+'))
+    for vertex in vertex_list:
+        if x != vertex.x:
+            L *= (X-vertex.x)/(x-vertex.x)
     return L * y
+
+
+def lagrange_interpolation_str(vertex_list: list[Vertex]):
+
+    result = ('-' * 10) + 'subPolinoms' + ('-' * 10) + '\n'
+
+    for vertex in vertex_list:
+        result += get_subpolinoms_str(vertex.x, vertex.y, vertex_list)
+
+    result = result[:-2] + '\n' + ('-' * 31)
+
+    return result
+
+
+def get_subpolinoms_str(x: float, y: float, vertex_list: list[Vertex]):
+
+    result = '('
+
+    for vertex in vertex_list:
+        if x != vertex.x:
+            result += get_subpolinom_str(x, vertex.x)
+
+    result = result[:-1] + ') * (' + str(y) + ') +\n'
+
+    return result
+
+
+def get_subpolinom_str(x, vertex_x):
+    return replace_nn_p(
+        ' ((x-' + str(vertex_x) + ')/(' + str(x) + '-' + str(vertex_x) + ')) *'
+    )
+
+
+def replace_nn_p(string: str): return string.replace('--', '+')
