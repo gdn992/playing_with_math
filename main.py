45ec5e756a7174d6fe87ff357aecdc33de885211
Gábor Németh <gabor.nemeth@avinty.com>
2023-05-20 23:36:55 +0200

    Added gui

--- main.py
+++ main.py
@@ -1,38 +1,14 @@
-import matplotlib.pyplot as plt
-from numpy import linspace
+import sys
+from PyQt5.QtWidgets import QApplication
 
-from lagrange import Point, lagrange_interpolation, lagrange_interpolation_str
-
-
-vertexes: list[Point] = [
-    Point(-4, -1),
-    Point(-3, -5),
-    Point(-2, 0),
-    Point(-1, -2),
-    Point(0, 0),
-    Point(1, 9),
-    Point(4, 12)
-]
+from src.view.mainWindow import MainWindow
 
 
-xp = [p.x for p in vertexes]
-yp = [p.y for p in vertexes]
+def window():
+    app = QApplication(sys.argv)
+    main_window = MainWindow()
+    main_window.show()
+    sys.exit(app.exec_())
 
 
-lagrange = lagrange_interpolation(vertexes)
-
-fromx = min(xp)
-tox = max(xp)
-
-fromy = min(yp)
-toy = max(yp)
-
-print(lagrange_interpolation_str(vertexes))
-
-linex = linspace(fromx, tox, 100)
-liney = [lagrange(x=x) for x in linex]
-
-
-plt.plot(linex, liney)
-plt.plot(xp, yp, 'ro')
-plt.show()
+window()
